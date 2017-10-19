1cd0e2c23f738175d861c151e231d2e40bef5ff2
CapedHero <mc.wrzesniewski@gmail.com>
2017-10-19 21:17:18 +0200

    Configure basic static setup.

--- crawler_haven/crawler_haven/settings.py
+++ crawler_haven/crawler_haven/settings.py
@@ -53,8 +53,7 @@ ROOT_URLCONF = 'crawler_haven.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,11 +113,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # User-uploaded media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Login settings
 
